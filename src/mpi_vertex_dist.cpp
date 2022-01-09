267e2c9b4b762eef486d2eeef0980218bef7aa5d
mabdullahsoyturk <msoyturk20@ku.edu.tr>
2022-01-09 17:11:57 +0300

    works and fast

--- src/mpi_vertex_dist.cpp
+++ src/mpi_vertex_dist.cpp
@@ -12,10 +12,6 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result) {
     int num_vertices = graph->num_vertices;
     fill_n(result, num_vertices, MAX_DIST);
 
-    int updated_neighbor_index = 0;
-    int *updated_neighbors_indices = new int[num_vertices];
-    int *updated_values = new int[num_vertices];
-
     auto start_time = Time::now();
 
     int depth = 0;
@@ -39,6 +35,9 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result) {
     while (keep_going) {
         keep_going = false;
 
+        int *num_sends = new int[num_ranks];
+        std::vector<int> send_indices;
+
         for (int vertex = my_start_vertex; vertex < my_end_vertex; vertex++) {
             if (result[vertex] == depth) {
                 for (int n = graph->v_adj_begin[vertex]; n < graph->v_adj_begin[vertex] + graph->v_adj_length[vertex]; n++) {
@@ -46,24 +45,55 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result) {
 
                     if (result[neighbor] > depth + 1) {
                         result[neighbor] = depth + 1;
-                        updated_neighbors_indices[updated_neighbor_index] = neighbor;
-                        updated_values[updated_neighbor_index] = depth + 1;
-                        updated_neighbor_index++;
+                        send_indices.push_back(neighbor);
                         keep_going = true;
                     }
                 }
             }
         }
 
-        MPI_Allreduce(MPI_IN_PLACE, result, num_vertices, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+        int num_send = send_indices.size();
+        int depths[num_send];
+        for(int i = 0; i < send_indices.size(); i++){
+            depths[i] = result[send_indices[i]];
+        }
+
+        //printf("My Rank: %d, num_send: %d\n", my_rank, num_send);
+
+        //printf("Rank: %d, Before num_send all gather\n", my_rank);
+        MPI_Allgather(&num_send, 1, MPI_INT, num_sends, 1, MPI_INT, MPI_COMM_WORLD);
+        /*for(int i = 0; i < num_ranks; i++) {
+            printf("Rank: %d, num_send: %d\n", i, num_sends[i]);
+        }*/
+        //printf("Rank: %d, After num_send all gather\n", my_rank);
+        int recv_size = 0;
+
+        int *displs = new int[num_ranks];
+        for(int i = 0; i < num_ranks; i++) {
+            displs[i] = recv_size;
+            recv_size += num_sends[i];
+        }
+
+        int *recv_indices = new int[recv_size];
+        int *recv_depths = new int[recv_size];
+        MPI_Allgatherv(send_indices.data(), num_send, MPI_INT, recv_indices, num_sends, displs, MPI_INT, MPI_COMM_WORLD);
+        MPI_Allgatherv(depths, num_send, MPI_INT, recv_depths, num_sends, displs, MPI_INT, MPI_COMM_WORLD);
+
+        for(int i = 0; i < recv_size; i++) {
+            result[recv_indices[i]] = recv_depths[i];
+        }
+    
+        //MPI_Allreduce(MPI_IN_PLACE, result, num_vertices, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
         MPI_Allreduce(MPI_IN_PLACE, &keep_going, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
 
+        delete[] num_sends;
+        delete[] displs;
+        delete[] recv_indices;
+        delete[] recv_depths;
+
         depth++;
     }
-
-    delete[] updated_neighbors_indices;
-    delete[] updated_values;
 
     // print_result(graph, result, depth);
     return std::chrono::duration_cast<us>(Time::now() - start_time).count();
